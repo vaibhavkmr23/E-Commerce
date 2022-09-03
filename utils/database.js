3eafc12aabedc9e41e1d2d2b1a685b53d9d382e3
vaibhav <vaibhavkumarjha46@gmail.com>
2022-09-03 10:58:48 +0530

    Defining sql Models

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,19 @@
-const mysql = require("mysql2");
+const Sequelize = require('sequelize')
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node_project',
-    password: '23101995'
+const sequelize = new Sequelize('node-complete', 'root', '23101995', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
+
+// const mysql = require("mysql2");
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node_project',
+//     password: '23101995'
+// });
+
+// module.exports = pool.promise();
