fcb447f22c75196fc6c16b68b33ae350e011d510
vaibhav <vaibhavkumarjha46@gmail.com>
2022-09-05 17:21:36 +0530

    Using Majic Association Methods

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,14 +15,13 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-
-    Product.create({
+    req.user.createProduct({
         title: title,
         price: price,
         imageUrl: imageUrl,
         description: description,
-        userId: req.user.id
-    }).then(result => {
+    })
+    .then(result => {
         // console.log(result);
         console.log("Created Product");
         res.redirect('/admin/products');
