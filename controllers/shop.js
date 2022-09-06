d16a80236d592bde8ca57830d2bcc22ef339c0f7
vaibhav <vaibhavkumarjha46@gmail.com>
2022-09-06 16:37:01 +0530

    storing Cartitems as Orderitems & Resetting the Cart & Fetching and Outputting Orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -115,16 +115,42 @@ exports.postCartDeleteProduct = (req, res, next) => {
         .catch(err => console.log(err));
 };
 
-exports.getOrders = (req, res, next) => {
-    res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders'
-    });
-}
-
-exports.getCheckout = () => {
-    res.render('shop/checkout', {
-        path: '/checkout',
-        pageTitle: 'Checkout'
+exports.postOrder = (req, res, next) => {
+    let fetchedCart;
+    req.user.getCart().then(cart => {
+        fetchedCart = cart;
+        return cart.getProducts();
     })
+        .then(products => {
+            return req.user.createOrder()
+                .then(order => {
+                    return order.addProducts(
+                        products.map(product => {
+                            product.orderItem = {
+                                quantity: product.cartItem.quantity
+                            };
+                            return product;
+                        })
+                    )
+                }).catch(err => console.log(err));
+        })
+        .then(result => {
+            return fetchedCart.setProducts(null)
+        })
+        .then(result => {
+            res.redirect('/orders');
+        })
+        .catch(err => console.log(err));
+};
+
+exports.getOrders = (req, res, next) => {
+    req.user.getOrders({include: ['products']})
+    .then(orders => {
+        res.render('shop/orders', {
+            path: '/orders',
+            pageTitle: 'Your Orders',
+            orders: orders
+        });
+    })
+    .catch(err => console.log(err));   
 }
