ebea0491c6660636c860aac3812f0317ded0a2e7
vaibhav <vaibhavkumarjha46@gmail.com>
2022-09-08 16:09:28 +0530

    Displaying Cart Items

--- controllers/shop.js
+++ controllers/shop.js
@@ -50,20 +50,15 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
     req.user.getCart()
-        .then(cart => {
-            return cart
-                .getProducts()
-                .then(products => {
-                    res.render('shop/cart', {
-                        path: '/cart',
-                        pageTitle: 'Your Cart',
-                        products: products
-                    });
-                })
-                .catch(err => console.log(err));
+        .then(products => {
+            res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your Cart',
+                products: products
+            });
         })
         .catch(err => console.log(err));
-}
+};
 
 exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
