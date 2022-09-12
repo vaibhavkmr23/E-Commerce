71a312bca7ed5a28b664fedfc2807e754d8b4011
vaibhav <vaibhavkumarjha46@gmail.com>
2022-09-12 16:21:29 +0530

    Fetching All Products

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,7 +2,8 @@ const Product = require('../models/product');
 
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll().then(products => {
+    Product.find().then(products => {
+        console.log(products);
         res.render('shop/product-list', {
             prods: products,
             pageTitle: 'All Products',
@@ -37,7 +38,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll().then(products => {
+    Product.find().then(products => {
         res.render('shop/index', {
             prods: products,
             pageTitle: 'Shop',
