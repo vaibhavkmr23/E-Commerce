47c66f1a69cb86fa40ed8a5c80cdedf24b65bd08
vaibhav <vaibhavkumarjha46@gmail.com>
2022-09-12 17:37:32 +0530

    Adding and Using a User Model

--- app.js
+++ app.js
@@ -16,17 +16,17 @@ const shopRoutes = require('./routes/shop');
 
 const errorControllers = require('./controllers/error');
 
-// const User = require('./models/user');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public'))); // for serving path of static files eg. css files
 
-// app.use((req, res, next) => {
-//     User.findById('6318903a1b086de1823927fe').then(user => {
-//         req.user = new User(user.name, user.email, user.cart, user._id);
-//         next();
-//     }).catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+    User.findById('631f200ddb3d90b8064e4c78').then(user => {
+        req.user = user;
+        next();
+    }).catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -35,6 +35,18 @@ app.use(errorControllers.get404);
 
 mongoose.connect('mongodb+srv://Vaibhav:23101995@cluster0.gsxn3bf.mongodb.net/shop')
     .then(result => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'Test',
+                    email: 'test@test.com',
+                    cart: {
+                        items: []
+                    }
+                })
+                user.save();
+            }
+        });
         app.listen(3000);
         console.log("Connected");
     }).catch(err => {
