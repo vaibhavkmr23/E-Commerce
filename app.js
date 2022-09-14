3bbcb5185bd1ab9e6d0cb6e608d728be40c65307
vaibhav <vaibhavkumarjha46@gmail.com>
2022-09-14 11:13:15 +0530

    Using Session Middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const mongoose = require("mongoose");
+const session = require('express-session');
 
 const app = express();
 
@@ -21,6 +22,8 @@ const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public'))); // for serving path of static files eg. css files
+app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false }));
+
 
 app.use((req, res, next) => {
     User.findById('631f200ddb3d90b8064e4c78').then(user => {
