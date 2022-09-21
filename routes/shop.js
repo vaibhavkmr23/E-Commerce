2d21fbd8e747295b896f6917d6dcab864431aa1c
vaibhav <vaibhavkumarjha46@gmail.com>
2022-09-21 16:51:02 +0530

    Downloading Files with Authentication

--- routes/shop.js
+++ routes/shop.js
@@ -29,4 +29,6 @@ router.post('/create-order', isAuth, shopController.postOrder);
 
 router.get("/orders" , isAuth, shopController.getOrders);
 
+router.get('/orders/:orderId', isAuth, shopController.getInvoice);
+
 module.exports = router;
