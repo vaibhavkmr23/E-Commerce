b61d826d747956d4e80fcf0fb807826284387d0a
vaibhav <vaibhavkumarjha46@gmail.com>
2022-09-22 15:38:42 +0530

    Sending & Handling Background Request

--- controllers/admin.js
+++ controllers/admin.js
@@ -179,8 +179,8 @@ exports.getProducts = (req, res, next) => {
         })
 }
 
-exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+    const prodId = req.params.productId;
     Product.findById(prodId)
         .then(product => {
             if (!product) {
@@ -191,11 +191,9 @@ exports.postDeleteProduct = (req, res, next) => {
         })
         .then(result => {
             console.log("Destroyed Product");
-            res.redirect('/admin/products');
+            res.status(200).json({ message: "Success!!" })
         }).catch(err => {
-            const error = new Error(err);
-            error.httpStatusCode = 500;
-            return next(error);
+            res.status(500).json({ message: "Deleteing Products Failed" });
         })
 
 };
